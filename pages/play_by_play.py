082e6b34d47f86019270ae37fd518dfe20777f11
Evan Colwell <e.colwell@draftkings.com>
2026-07-09 12:29:36 -0700

    Fix play-by-play filters to only show relevant play types

--- pages/play_by_play.py
+++ pages/play_by_play.py
@@ -19,19 +19,29 @@ def render(
     color_map = resolve_team_colors(game_away_abbrev, game_home_abbrev)
 
     # Filter controls
-    col1, col2 = st.columns([2, 1])
-    with col1:
-        all_teams = sorted({d.team.abbreviation for d in drives})
-        team_filter = st.multiselect(
-            "Filter by team", options=["All"] + all_teams, default=["All"],
-            label_visibility="collapsed",
-        )
-    with col2:
-        type_options = sorted({p.play_type.value for d in drives for p in d.plays})
-        type_filter = st.multiselect(
-            "Filter by play type", options=["All"] + type_options, default=["All"],
-            label_visibility="collapsed",
-        )
+    all_teams = sorted({d.team.abbreviation for d in drives})
+    team_filter = st.multiselect(
+        "Filter by team", options=["All"] + all_teams, default=["All"],
+        label_visibility="collapsed",
+    )
+
+    PLAY_TYPE_FILTERS = [
+        "Fumble",
+        "Field Goal Good",
+        "Field Goal Missed",
+        "Pass Incompletion",
+        "Pass Interception Return",
+        "Pass Reception",
+        "Passing Touchdown",
+        "Penalty",
+        "Punt",
+        "Rush",
+        "Sack",
+    ]
+    type_filter = st.multiselect(
+        "Filter by play type", options=["All"] + PLAY_TYPE_FILTERS, default=["All"],
+        label_visibility="collapsed",
+    )
 
     rows = []
     for drive in drives:
